b3b517b7cede699c3ffc56da083c151d91b05323
Stas Remnev <remnev.stas@gmail.com>
2022-04-05 18:06:19 +0300

    style: full block

---
+++ js/index.js
@@ -0,0 +1,33 @@
+const image = document.querySelector('.product__images-main');
+const popupImg = document.querySelector('.image-popup');
+
+const photo = document.querySelector('.image-popup__photo');
+const popup = document.querySelector('.popup');
+
+image.addEventListener('click', () => {
+  photo.src = image.src;
+  // console.log(photo);
+  openPopup(popupImg);
+});
+
+const openPopup = (popups) => {
+  popups.classList.add('popup_opened');
+  window.addEventListener('keydown', closeByEscape);
+};
+const closePopup = (popups) => {
+  popups.classList.remove('popup_opened');
+  window.removeEventListener('keydown', closeByEscape);
+};
+
+const closeByEscape = (evt) => {
+  if (evt.key === 'Escape') {
+    const openedPopup = document.querySelector('.popup_opened');
+    closePopup(openedPopup);
+  }
+};
+
+popup.addEventListener('mousedown', (evt) => {
+  if (evt.target.classList.contains('popup_opened')) {
+    closePopup(popup);
+  }
+});
